422c0ba7bd90f27a59ec07101d29ea843e4b70a9
MirQ-hub <n2vami00@students.oamk.fi>
2023-03-30 12:14:51 +0300

    verkkopalveluprojekti

---
+++ src/components/nav.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Logo from '../Oamk simpanssit.png'
+function nav() {
+  return (
+    <div className='navbar'>
+        <div className='leftSide'>
+            <img src={Logo} alt=""/>
+        </div>
+        <div className='rightSide'></div>
+         </div>
+  )
+}
+
+export default nav
