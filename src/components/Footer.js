4a8c04e3e3d3128bb3fee6e2a999a154c7b0a854
MirQ-hub <n2vami00@students.oamk.fi>
2023-04-13 15:19:04 +0300

    Lisäilin kuvia

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,17 +1,22 @@
 import React from "react";
 import '../styles/footer.css'
 import Avatar from '@mui/material/Avatar';
+import monkey from "../img/monkey1.png"
+import monkey1 from "../img/monkey .png"
+import monkey2 from "../img/monkey2.png"
+import monkey5 from "../img/monkey6.png"
+import monkey4 from "../img/monkey5.png"
 
 function Footer() {
   return (
     <div className="footer">
       <div className="avatar">
  
-      <Avatar alt="Elina" src="/static/images/avatar/1.jpg" />
-      <Avatar alt="Aleksi" src="/static/images/avatar/2.jpg" />
-      <Avatar alt="Tommi" src="/static/images/avatar/3.jpg" />
-      <Avatar alt="Nikita" src="/static/images/avatar/3.jpg" />
-      <Avatar alt="Mirva" src="/static/images/avatar/3.jpg" />
+      <Avatar alt="Elina" src={monkey} />
+      <Avatar alt="Aleksi" src={monkey1} />
+      <Avatar alt="Tommi" src={monkey2} />
+      <Avatar alt="Nikita" src={monkey5} />
+      <Avatar alt="Mirva" src={monkey4} />
         </div> 
 
       <p> &copy;  <b>2023  Verkkopalveluprojekti</b>  Aleksi, Elina, Mirva, Nikita, Tommi  </p>
